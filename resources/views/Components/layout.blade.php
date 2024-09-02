82e6979c865335549585cada639fae07c6490e9f
Addd12 <addd.github@gmail.com>
2024-09-02 19:06:34 +0200

    completed day 3, routes, views, components

---
+++ resources/views/Components/layout.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home</title>
+</head>
+<body>
+    <nav>
+        <a href="/home">home</a>
+        <a href="/about">about</a>
+        <a href="/contact">contact</a>
+        <a href="/meet-the-team">team</a>
+    </nav>
+    <!-- we can use double brackets as below instead of the php tag -->
+    {{$slot}}
+</body>
+</html>
