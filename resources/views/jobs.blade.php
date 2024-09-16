9b5820db6137ac453f60226460ba7f85184b2f4b
Addd12 <addd.github@gmail.com>
2024-09-16 17:23:55 +0200

    d13 eager loading and N+1 problem

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -1,11 +1,16 @@
 <x-layout>
     <x-slot:heading>Jobs</x-slot>
-        <h1>jobs board</h1>
-        <ul>
+        <h1 class="text-center font-light text-lg">Jobs board</h1>
+        <div class="space-y-4">
             @foreach ($jobs as $job)
-                <a href="/jobs/{{$job['id']}}" class="text-blue-500 hover:underline">
-                    <li>Job: {{$job['title']}} -- Salary: ${{$job['salary']}}</li>
+                <a href="/jobs/{{$job['id']}}" class="block px-4 py-6 border border-gray-200 rounded-lg">
+                <div class="font-bold text-blue-500 text-sm">
+                    {{$job->employer->name}}
+                </div>    
+                <div>
+                        <strong>Job: {{$job['title']}}</strong> Salary: {{$job['salary']}}
+                    </div>
                 </a>
             @endforeach
-        </ul>
+        </div>
 </x-layout>
